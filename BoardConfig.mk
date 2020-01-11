ba8cd5db22443d1f1a4dd2f7783f95b44c7befde
ShivamKumarJha <jha.shivam3@gmail.com>
2020-01-11 16:17:39 +0000

    sdm660_64-user 8.1.0 OPM1 478 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/asus/X00TD
+BOARD_VENDOR := asus
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/asus/X00TD/BoardConfigVendor.mk
